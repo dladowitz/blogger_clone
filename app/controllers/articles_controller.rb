afcbdf1f1731243f95981e27946298fa1cdfdb00
David Ladowitz <david@ladowitz.com>
2012-07-25 00:04:29 -0700

    stuff

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  before_filter :require_login, :except => [:index, :show]
+  
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    
+    audit @article.inspect
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(:title => params[:article][:title], :tag_list => params[:article][:tag_list], :body => params[:article][:body])
+    puts "Here are the params you ordered: #{params}"
+    warn "This is the object after initialization: #{@article.inspect}"
+    
+    audit @article.inspect
+    # binding.pry
+    
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+    
+    audit @article.inspect
+    
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    audit @article.inspect
+
+    flash[:message] = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,7 @@
+module ApplicationHelper
+  
+  def d(object)
+    debug object
+  end
+  
+end
